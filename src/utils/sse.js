d9b932a954a99a9092e31a810061afac234c4b86
navetacandra <naveta.cand@gmail.com>
2024-02-10 17:20:15 +0700

    feat: add sse helper

---
+++ src/utils/sse.js
@@ -0,0 +1,19 @@
+function setHeader(req, res, closeCallback) {
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive"
+  });
+  res.flushHeaders();
+
+  req.on('close', () => {
+    res.end();
+    closeCallback();
+  })
+}
+
+function writeResponse(res, data) {
+  res.write(`data: ${JSON.stringify(data)}\n\n`);
+}
+
+export { setHeader, writeResponse };
