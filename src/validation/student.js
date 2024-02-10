91d3e39cba5655d0bec412989ac760d387040a53
navetacandra <naveta.cand@gmail.com>
2024-02-10 17:18:33 +0700

    fix: update student validation

--- src/validation/student.js
+++ src/validation/student.js
@@ -1,19 +1,21 @@
 import Joi from "joi";
 
 const studentIdValidation = Joi.string().required();
+const phonenumberValidation = Joi.string().pattern(/^\d{10,14}$/).required();
 const studentValidation = Joi.object({
   nis: Joi.string().pattern(/^\d+$/).required(),
   nama: Joi.string().required(),
   email: Joi.string().email().required(),
   kelas: Joi.string().required(),
   alamat: Joi.string().required(),
-  telSiswa: Joi.string().pattern(/^\d{10,14}+$/).required(),
-  telWaliMurid: Joi.string().pattern(/^\d{10,14}+$/).required(),
-  telWaliKelas: Joi.string().pattern(/^\d{10,14}+$/).required(),
+  telSiswa: phonenumberValidation,
+  telWaliMurid: phonenumberValidation,
+  telWaliKelas: phonenumberValidation,
   card: Joi.string().pattern(/^(-\w{2}){4}$/)
 }).options({ stripUnknown: true });
 
 export {
   studentIdValidation,
+  phonenumberValidation,
   studentValidation
 };
