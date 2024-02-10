82b1ce630751bbc8779c6c15e4580c7fd2d7dcc1
navetacandra <naveta.cand@gmail.com>
2024-02-10 17:21:42 +0700

    feat: add express server

---
+++ src/main.js
@@ -0,0 +1,47 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { resolve, join, dirname } from "path";
+import { fileURLToPath } from "url";
+import addInstance from "./middleware/add-instance.js";
+import routerV1 from "./routes/router-v1.js";
+import MongoService from "./services/mongo-service.js";
+import WhatsApp from "./services/whatsapp-service.js";
+import Queue from "./utils/queue.js";
+import logger from "./utils/logger.js";
+
+const __dirname = resolve(dirname(fileURLToPath(import.meta.url)));
+let queue, mongo, whatsapp, web;
+
+async function exitHandler(exitCode) {
+  try {
+    logger.info('MongoDB saving state..');
+    await mongo?.saveToDatabase();
+    logger.info('MongoDB state saved.');
+
+    logger.info('Saving queue..');
+    queue?.saveQueue();
+    logger.info('Queue saved.');
+  } catch(err) {
+    logger.error(`Failed run exitHandler. caused: ${err}`);
+  }
+
+  process.exit(isNaN(+exitCode) ? 1 : +exitCode);
+};
+
+["SIGINT", "SIGTERM", "unhandledRejection", "uncaughtException", "exit"]
+  .forEach(ev => process.on(ev, exitHandler));
+
+(async function() {
+  queue = new Queue(resolve(join(__dirname, '../queue.json')));
+  mongo = new MongoService({ appName: 'presence', mongoUrl: "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.3" });
+  whatsapp = new WhatsApp({ appName: 'presence' });
+  web = express();
+
+  web.use(bodyParser.urlencoded({ extended: true }));
+  web.use(bodyParser.json());
+  web.use((req, res, next) => addInstance(req, res, next, { instance: mongo, name: 'mongo' }));
+  web.use((req, res, next) => addInstance(req, res, next, { instance: whatsapp, name: 'whatsapp' }));
+
+  web.use('/api/v1', routerV1);
+  web.listen(3000, () => console.log('Running..'));
+})();
---
+++ src/routes/router-v1.js
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { modeGet, modeUpdate, scheduleDetailGet, scheduleDetailUpdate, scheduleGet, scheduleUpdate } from "../controller/schedule.js";
+import { readFileSync } from "fs";
+import { studentDeleteById, studentGetById, studentPutById, studentsGet, studentsPost } from "../controller/student.js";
+import { onWhatsappGet, whatsappGet, whatsappLogout } from "../controller/whatsapp.js";
+
+const routerV1 = new Router();
+
+const apiSpecPath = path.resolve(path.join(path.dirname(fileURLToPath(import.meta.url)), '../..', 'api-spec.json'));
+const swaggerDefinition = JSON.parse(readFileSync(apiSpecPath, 'utf8'));
+const specs = swaggerJSDoc({
+  swaggerDefinition,
+  apis: [ apiSpecPath ]
+});
+
+routerV1.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
+routerV1.get('/mode', modeGet);
+routerV1.put('/mode', modeUpdate);
+routerV1.get('/schedule-detail', scheduleDetailGet);
+routerV1.put('/schedule-detail', scheduleDetailUpdate);
+routerV1.get('/schedule', scheduleGet);
+routerV1.put('/schedule', scheduleUpdate);
+routerV1.get('/students', studentsGet)
+routerV1.post('/students', studentsPost);
+routerV1.get('/student/:studentId', studentGetById);
+routerV1.put('/student/:studentId', studentPutById);
+routerV1.delete('/student/:studentId', studentDeleteById);
+routerV1.get('/whatsapp', whatsappGet);
+routerV1.get('/whatsapp/logout', whatsappLogout);
+routerV1.get('/on-whatsapp/:number', onWhatsappGet);
+
+export { routerV1 as default };
