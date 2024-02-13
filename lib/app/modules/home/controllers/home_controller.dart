9ddce24591b940900f2635127d39161252e45f4b
navetacandra <naveta.cand@gmail.com>
2024-02-14 06:33:02 +0700

    feat: create proto home page

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,7 +1,8 @@
+import 'package:attendance_app/app/controllers/http_controller.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  // final streamStudent = HttpController.streamList("/students");
 
   final count = 0.obs;
   @override
@@ -17,6 +18,7 @@ class HomeController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+    // streamStudent.close();
   }
 
   void increment() => count.value++;
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,22 +1,57 @@
+import 'package:attendance_app/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+  HomeView({super.key});
+  final PanelController _pc = PanelController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) {
+        final action = _pc.isPanelOpen ? _pc.close : Get.back;
+        action();
+      },
+      child: SlidingUpPanel(
+        defaultPanelState: PanelState.CLOSED,
+        controller: _pc,
+        isDraggable: false,
+        minHeight: 0,
+        maxHeight: Get.height * 0.8,
+        panel: Container(
+          decoration: const BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+        ),
+        body: Scaffold(
+          body: SafeArea(
+            child: Wrap(
+              direction: Axis.horizontal,
+              children: <Widget>[
+                ElevatedButton(
+                  child: const Text("STUDENTS"),
+                  onPressed: () {
+                    Get.toNamed(Routes.STUDENTS);
+                    // _pc.open();
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text("CARDS"),
+                  onPressed: () {
+                    Get.toNamed(Routes.CARDS);
+                    // _pc.open();
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
