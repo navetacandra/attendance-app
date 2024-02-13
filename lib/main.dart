3ea7c1c901150da9af3c2ed8838f8a44fbd708ef
navetacandra <naveta.cand@gmail.com>
2024-02-14 06:30:22 +0700

    feat: add theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
@@ -14,9 +15,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Attendance App",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        useMaterial3: true,
+      ),
     );
   }
 }
