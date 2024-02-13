91d0b85ce5da3ba683caae8f03585deb90ff4611
navetacandra <naveta.cand@gmail.com>
2024-02-14 06:29:43 +0700

    feat: add routes

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,17 @@
 import 'package:get/get.dart';
 
+import '../modules/add_student/bindings/add_student_binding.dart';
+import '../modules/add_student/views/add_student_view.dart';
+import '../modules/cards/bindings/cards_binding.dart';
+import '../modules/cards/views/cards_view.dart';
+import '../modules/edit_student/bindings/edit_student_binding.dart';
+import '../modules/edit_student/views/edit_student_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/pick_student/bindings/pick_student_binding.dart';
+import '../modules/pick_student/views/pick_student_view.dart';
+import '../modules/students/bindings/students_binding.dart';
+import '../modules/students/views/students_view.dart';
 
 part 'app_routes.dart';
 
@@ -13,8 +23,33 @@ class AppPages {
   static final routes = [
     GetPage(
       name: _Paths.HOME,
-      page: () => const HomeView(),
+      page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.STUDENTS,
+      page: () => StudentsView(),
+      binding: StudentsBinding(),
+    ),
+    GetPage(
+      name: _Paths.ADD_STUDENT,
+      page: () => AddStudentView(),
+      binding: AddStudentBinding(),
+    ),
+    GetPage(
+      name: _Paths.EDIT_STUDENT,
+      page: () => EditStudentView(),
+      binding: EditStudentBinding(),
+    ),
+    GetPage(
+      name: _Paths.CARDS,
+      page: () => CardsView(),
+      binding: CardsBinding(),
+    ),
+    GetPage(
+      name: _Paths.PICK_STUDENT,
+      page: () => PickStudentView(),
+      binding: PickStudentBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,9 +4,19 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const STUDENTS = _Paths.STUDENTS;
+  static const ADD_STUDENT = _Paths.ADD_STUDENT;
+  static const EDIT_STUDENT = _Paths.EDIT_STUDENT;
+  static const CARDS = _Paths.CARDS;
+  static const PICK_STUDENT = _Paths.PICK_STUDENT;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
+  static const STUDENTS = '/students';
+  static const ADD_STUDENT = '/add-student';
+  static const EDIT_STUDENT = '/edit-student';
+  static const CARDS = '/cards';
+  static const PICK_STUDENT = '/pick-student';
 }
