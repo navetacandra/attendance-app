6845b5b8a3b357f28ce85936e1ae4b6f3c72a390
navetacandra <naveta.cand@gmail.com>
2024-02-25 13:48:35 +0700

    fix: modify home page

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,4 +1,31 @@
+import 'dart:async';
+import 'package:attendance_app/app/controllers/http_controller.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
+  StreamController streamSiswa = HttpController.streamList("/students");
+  StreamController streamAttended = HttpController.streamList("/presence");
+  RxInt siswa = 0.obs;
+  RxDouble percentage = 0.0.obs;
+
+  @override
+  void onInit() {
+    streamSiswa.stream.listen((siswaList) {
+      siswa.value = siswaList.length;
+    });
+    streamAttended.stream.listen((attendedList) {
+      percentage.value = attendedList.length / siswa.value;
+      if(percentage.isNaN || percentage.isInfinite) {
+        percentage.value = 0;
+      }
+    });
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    streamSiswa.close();
+    streamAttended.close();
+    super.onClose();
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,74 +1,288 @@
 import 'package:attendance_app/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sliding_up_panel/sliding_up_panel.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   HomeView({super.key});
-  final PanelController _pc = PanelController();
+  final selfC = Get.find<HomeController>();
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
-      onPopInvoked: (didPop) {
-        final action = _pc.isPanelOpen ? _pc.close : Get.back;
-        action();
-      },
-      child: SlidingUpPanel(
-        defaultPanelState: PanelState.CLOSED,
-        controller: _pc,
-        isDraggable: false,
-        minHeight: 0,
-        maxHeight: Get.height * 0.8,
-        panel: Container(
-          decoration: const BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Stack(
+          children: <Widget>[
+            SizedBox(
+              height: Get.height * .33,
+              width: double.infinity,
+              child: Stack(
+                children: <Widget>[
+                  Align(
+                    alignment: const Alignment(0, -1),
+                    child: Container(
+                      width: double.infinity,
+                      height: Get.height * .32,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Align(
+                    alignment: const Alignment(0, 1),
+                    child: Container(
+                      width: double.infinity,
+                      height: Get.height * .025,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(Get.width * .5),
+                          topRight: Radius.circular(Get.width * .5)
+                        ),
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ),
-        body: Scaffold(
-          body: SafeArea(
-            child: Wrap(
-              direction: Axis.horizontal,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                ElevatedButton(
-                  child: const Text("STUDENTS"),
-                  onPressed: () {
-                    Get.toNamed(Routes.STUDENTS);
-                  },
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: double.infinity,
+                  height: Get.height * .20,
+                  // color: Colors.red,
                 ),
-                ElevatedButton(
-                  child: const Text("CARDS"),
-                  onPressed: () {
-                    Get.toNamed(Routes.CARDS); 
-                  },
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  width: Get.width * .9,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(.5),
+                        spreadRadius: 5,
+                        blurRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        width: Get.width*.4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            const Text(
+                              "Jumlah Siswa/i",
+                              softWrap: true,
+                              maxLines: 2,
+                              textAlign: TextAlign.center,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                            const SizedBox(height: 18),
+                            Obx(() =>
+                              Text(
+                                "${selfC.siswa.value}",
+                                textAlign: TextAlign.center,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            const Text(
+                              "Siswa/i Terdaftar",
+                              softWrap: true,
+                              maxLines: 2,
+                              textAlign: TextAlign.center,
+                              overflow: TextOverflow.clip,
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 16,
+                              )
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: Get.width*.4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            const Text(
+                              "Persentase Kehadiran",
+                              softWrap: true,
+                              maxLines: 2,
+                              textAlign: TextAlign.center,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                            const SizedBox(height: 18),
+                            Obx(() =>
+                              Stack(
+                                children: <Widget>[
+                                  Align(
+                                    alignment: const Alignment(0, 0),
+                                    child: SizedBox(
+                                      width: 60,
+                                      height: 60,
+                                      child: CircularProgressIndicator(
+                                        value: selfC.percentage.value,
+                                        color: Colors.blue,
+                                        backgroundColor: Colors.grey,
+                                        strokeWidth: 5,
+                                      ),
+                                    ),
+                                  ),
+                                  Align(
+                                    alignment: const Alignment(0, 0),
+                                    child: Container(
+                                      margin: const EdgeInsets.only(top: 19),
+                                      child: Text(
+                                        "${selfC.percentage.value * 100}%",
+                                        overflow: TextOverflow.clip,
+                                        style: const TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
+                                        )
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ]
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                ElevatedButton(
-                  child: const Text("WHATSAPP"),
-                  onPressed: () {
-                    Get.toNamed(Routes.WHATSAPP); 
-                  },
-                ),
-                ElevatedButton(
-                  child: const Text("SCHEDULE"),
-                  onPressed: () {
-                    Get.toNamed(Routes.SCHEDULE); 
-                  },
-                ),
-                ElevatedButton(
-                  child: const Text("REPORT"),
-                  onPressed: () {
-                    Get.toNamed(Routes.REPORT); 
-                  },
+                const SizedBox(height: 30),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 15),
+                  width: Get.width * .9,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(.5),
+                        spreadRadius: 5,
+                        blurRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: Wrap(
+                    direction: Axis.horizontal,
+                    alignment: WrapAlignment.start,
+                    runSpacing: 20,
+                    children: <Widget>[
+                      menuItem(
+                        label: "Tambah Siswa",
+                        iconPath: "assets/user-add.png",
+                        navigation: Routes.STUDENTS,
+                      ),
+                      menuItem(
+                        label: "Tambah Kartu", 
+                        iconPath: "assets/card.png", 
+                        navigation: Routes.CARDS,
+                      ),
+                      menuItem(
+                        label: "Download Laporan", 
+                        iconPath: "assets/note.png", 
+                        navigation: Routes.REPORT,
+                      ),
+                      menuItem(
+                        label: "Jadwal Absensi",
+                        iconPath: "assets/calendar-clock.png", 
+                        navigation: Routes.SCHEDULE,
+                      ),
+                      menuItem(
+                        label: "Koneksi WhatsApp",
+                        iconPath: "assets/phone.png",
+                        navigation: Routes.WHATSAPP,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-          ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Container menuItem({
+    required String label,
+    required String iconPath, 
+    required String navigation
+  }) {
+    return Container(
+      width: Get.width * .17,
+      margin: EdgeInsets.symmetric(horizontal: Get.width * .024),
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(3),
+            margin: const EdgeInsets.only(bottom: 5),
+            child: InkWell(
+              onTap: () => Get.toNamed(navigation),
+              child: Container(
+                width: Get.width*.12,
+                height: Get.width*.12,
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.white70,
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(.5),
+                      spreadRadius: 2,
+                      blurRadius: 2,
+                    ),
+                  ],
+                ),
+                child: Image.asset(
+                  iconPath,
+                  width: Get.width*.12,
+                  height: Get.width*.12,
+                ),
+              ),
+            ),
+          ),
+          Text(
+            label,
+            softWrap: true,
+            maxLines: 2,
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
       ),
     );
   }
